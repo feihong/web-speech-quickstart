7e41c81a41a08da1f50a369214b227709482909e
Feihong Hsu <feihong.hsu@gmail.com>
2018-02-21 10:26:33 -0600

    Add more buttons

--- main.js
+++ main.js
@@ -11,13 +11,33 @@ function populateVoiceList() {
 populateVoiceList()
 speechSynthesis.onvoiceschanged = populateVoiceList
 
-function speak() {
-  let utter = new SpeechSynthesisUtterance('Hello World')
+function speak(text) {
+  let phrase = 'Hello World'
+  if (typeof text === 'string') {
+    phrase = text
+  }
+  let utter = new SpeechSynthesisUtterance(phrase)
   speechSynthesis.speak(utter)
 }
 
 $('.speak').on('click', speak)
 
-$('.speak-callback').on('click', () => {
-  setTimeout(speak, 100)
+$('.speak-delay').on('click', () => {
+  setTimeout(speak, 2000)
+})
+
+$('.speak-ajax').on('click', () => {
+  $.getJSON('/data.json', data => {
+    speak(data.text)
+  })
+})
+
+$('.speak-ajax-delay').on('click', () => {
+  $.getJSON('/data.json', data => {
+    setTimeout(() => speak(data.text), 2000)
+  })
+})
+
+$('.speak-repeat').on('click', () => {
+  setInterval(speak, 2000)
 })
